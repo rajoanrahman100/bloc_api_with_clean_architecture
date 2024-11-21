fae185dbce4c5260de27239d9c3174e16dc492f8
sahik1234 <sahikahtesam1994@gmail.com>
2024-11-21 14:17:33 +0600

    splash: authentication logic is done

---
+++ lib/presentation/splash/block/splash_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc_api_with_clean_architecture/presentation/splash/block/splash_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashCubit extends Cubit<SplashState> {
+  SplashCubit() : super(DisplaySplash()); //Set the initial state
+
+  void appStarted()async{
+    await Future.delayed(const Duration(seconds: 2));
+    emit(Authenticated());
+  }
+
+}
--- lib/presentation/splash/page/splash.dart
+++ lib/presentation/splash/page/splash.dart
@@ -1,5 +1,11 @@
+import 'package:bloc_api_with_clean_architecture/common/helper/navigation/app_navigation.dart';
 import 'package:bloc_api_with_clean_architecture/core/configs/assets/app_images.dart';
+import 'package:bloc_api_with_clean_architecture/presentation/auth/page/signin.dart';
+import 'package:bloc_api_with_clean_architecture/presentation/home/page/home.dart';
+import 'package:bloc_api_with_clean_architecture/presentation/splash/block/splash_cubit.dart';
+import 'package:bloc_api_with_clean_architecture/presentation/splash/block/splash_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -7,23 +13,30 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.splashBackground))),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.center,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    const Color(0xff1A1B20).withOpacity(0),
-                    const Color(0xff1A1B20),
-              ])
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is UnAuthenticated) {
+            //Redirect user to Login page
+            AppNavigator.pushReplacement(context, const SignInPage());
+          }
+          if (state is Authenticated) {
+            AppNavigator.pushReplacement(context, const HomePage());
+          }
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(image: DecorationImage(image: AssetImage(AppImages.splashBackground))),
             ),
-          )
-        ],
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(begin: Alignment.center, end: Alignment.bottomCenter, colors: [
+                const Color(0xff1A1B20).withOpacity(0),
+                const Color(0xff1A1B20),
+              ])),
+            )
+          ],
+        ),
       ),
     );
   }
